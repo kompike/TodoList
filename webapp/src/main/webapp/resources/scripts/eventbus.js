8a97e7c5d6b5395abea1360f1ba311284a0c2795
kompike <kompanietskostya@gmail.com>
2016-08-04 00:51:49 +0300

    JavaScript: event bus implemented.

---
+++ webapp/src/main/webapp/resources/scripts/eventbus.js
@@ -0,0 +1,52 @@
+var EventBus = function(){
+
+    var _subscribers = {};
+
+    var _post = function(eventType, someEvent) {
+
+        var eventTypeSubscribers = _subscribers[eventType];
+
+        if (eventTypeSubscribers == undefined) {
+
+            console.log("Subscribers not found for eventType : " + eventType);
+
+        } else {
+
+            for (var i = 0; i < eventTypeSubscribers.length; i++) {
+                eventTypeSubscribers[i](someEvent);
+            }
+        }
+    };
+
+    var _subcribe = function(eventType, callback) {
+
+        if (typeof (_subscribers[eventType]) === 'undefined') {
+
+            _subscribers[eventType] = [];
+
+        }
+
+        if (typeof (callback) === 'function') {
+
+            _subscribers[eventType].push(callback);
+
+        } else {
+
+            console.log("Callback must be function for event type : " + eventType);
+
+        }
+    };
+
+    return {
+        "post": _post,
+        "subscribe": _subcribe
+    };
+}
+
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+
+define(function() {
+    return EventBus;
+});
---
+++ webapp/src/main/webapp/resources/scripts/todolist.js
@@ -0,0 +1,3 @@
+/**
+ * Created by kompike on 04.08.2016.
+ */

b900ede4a267f23fe0effca01e910fdfd9679abb
kompike <kompanietskostya@gmail.com>
2016-08-04 00:24:54 +0300

    Web module: Dispatcher servlet implemented.

--- webapp/src/main/java/com/javaclasses/todolist/webapp/DispatcherServlet.java
+++ webapp/src/main/java/com/javaclasses/todolist/webapp/DispatcherServlet.java
@@ -1,6 +1,8 @@
 package com.javaclasses.todolist.webapp;
 
 
+import com.javaclasses.todolist.webapp.handler.Handler;
+import com.javaclasses.todolist.webapp.handler.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 /**
  * Dispatcher servlet which handles all user's requests
@@ -16,6 +21,8 @@ import java.io.IOException;
 public class DispatcherServlet extends HttpServlet {
 
     private final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+
+    private final HandlerRegistry registry = HandlerRegistry.getInstance();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -29,5 +36,29 @@ public class DispatcherServlet extends HttpServlet {
 
     private void execute(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
+
+        if (log.isInfoEnabled()) {
+            log.info("Start executing user's request...");
+        }
+
+        final String uri = request.getRequestURI();
+        final String method = request.getMethod().toUpperCase();
+        final RequestContext requestContext = new RequestContext(uri, method);
+
+        final Handler handler = registry.getHandler(requestContext);
+        final JsonEntity jsonEntity = handler.process(request, response);
+        final PrintWriter printWriter = response.getWriter();
+        response.setContentType(APPLICATION_JSON);
+        printWriter.write(jsonEntity.generateJson());
+
+        if (log.isInfoEnabled()) {
+            log.info("Generated response: " + jsonEntity.generateJson());
+        }
+
+        response.setStatus(jsonEntity.getResponseStatusCode());
+
+        if (log.isInfoEnabled()) {
+            log.info("Response successfully created.");
+        }
     }
 }

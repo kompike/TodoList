b87049cc2f195d9fe1986d3b0730cd7b17c6b16d
kompike <kompanietskostya@gmail.com>
2016-08-04 00:38:12 +0300

    Web module: Context listener added.

--- model/src/test/java/com/javaclasses/todolist/model/UserServiceShould.java
+++ model/src/test/java/com/javaclasses/todolist/model/UserServiceShould.java
@@ -1,12 +1,9 @@
 package com.javaclasses.todolist.model;
 
-import com.javaclasses.todolist.model.dto.LoginDTO;
 import com.javaclasses.todolist.model.dto.RegistrationDTO;
-import com.javaclasses.todolist.model.dto.SecurityTokenDTO;
 import com.javaclasses.todolist.model.dto.UserDTO;
 import com.javaclasses.todolist.model.entity.tinytype.Email;
 import com.javaclasses.todolist.model.entity.tinytype.UserId;
-import com.javaclasses.todolist.model.service.UserAuthenticationException;
 import com.javaclasses.todolist.model.service.UserRegistrationException;
 import com.javaclasses.todolist.model.service.UserService;
 import com.javaclasses.todolist.model.service.impl.UserServiceImpl;
@@ -108,60 +105,6 @@ public class UserServiceShould {
         } catch (UserRegistrationException ex) {
             assertEquals("Wrong message for already existing user.",
                     "User with given email already exists", ex.getMessage());
-
-            userService.delete(userId);
-        }
-    }
-
-    @Test
-    public void allowRegisteredUserToLogin()
-            throws UserRegistrationException, UserAuthenticationException {
-
-        final UserId userId =
-                userService.register(new RegistrationDTO(email, password, password));
-        final UserDTO userDTO = userService.findById(userId);
-
-        assertEquals("Actual email of registered user does not equal expected.",
-                email, userDTO.getEmail());
-
-        final SecurityTokenDTO tokenDTO = userService.login(new LoginDTO(email, password));
-        final UserDTO loggedUser = userService.findByToken(tokenDTO.getTokenId());
-
-        assertEquals("Actual email of logged user does not equal expected.",
-                email, loggedUser.getEmail());
-
-        userService.delete(userId);
-    }
-
-    @Test
-    public void prohibitLoginOfNotRegisteredUser() {
-
-        try {
-            userService.login(new LoginDTO(email, password));
-            fail("Not registered user logged in.");
-        } catch (UserAuthenticationException ex) {
-            assertEquals("Wrong message for not registered user.",
-                    "Incorrect email/password", ex.getMessage());
-        }
-    }
-
-    @Test
-    public void prohibitLoginOfUserWithIncorrectPassword()
-            throws UserRegistrationException {
-
-        final UserId userId =
-                userService.register(new RegistrationDTO(email, password, password));
-        final UserDTO userDTO = userService.findByEmail(new Email(email));
-
-        assertEquals("Actual email of registered user does not equal expected.",
-                email, userDTO.getEmail());
-
-        try {
-            userService.login(new LoginDTO(email, "pass"));
-            fail("User with incorrect password logged in.");
-        } catch (UserAuthenticationException ex) {
-            assertEquals("Wrong message for incorrect password.",
-                    "Incorrect email/password", ex.getMessage());
 
             userService.delete(userId);
         }
---
+++ webapp/src/main/java/com/javaclasses/todolist/webapp/listener/ContextListener.java
@@ -0,0 +1,20 @@
+package com.javaclasses.todolist.webapp.listener;
+
+import com.javaclasses.todolist.webapp.controller.UserController;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Context listener for available controllers initialization
+ */
+public class ContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        UserController.init();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {}
+}

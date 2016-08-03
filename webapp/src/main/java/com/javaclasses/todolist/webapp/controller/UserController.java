27832cca7ff6020ec5c3ef10eff5eec43ac8c32e
kompike <kompanietskostya@gmail.com>
2016-08-04 01:58:40 +0300

    User login: Login handler implemented.

--- webapp/src/main/java/com/javaclasses/todolist/webapp/controller/UserController.java
+++ webapp/src/main/java/com/javaclasses/todolist/webapp/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.javaclasses.todolist.webapp.controller;
 
+import com.javaclasses.todolist.model.dto.LoginDTO;
 import com.javaclasses.todolist.model.dto.RegistrationDTO;
+import com.javaclasses.todolist.model.dto.SecurityTokenDTO;
+import com.javaclasses.todolist.model.service.UserAuthenticationException;
 import com.javaclasses.todolist.model.service.UserRegistrationException;
 import com.javaclasses.todolist.model.service.UserService;
 import com.javaclasses.todolist.model.service.impl.UserServiceImpl;
@@ -24,6 +27,7 @@ public class UserController {
 
     // Possible URLs
     private static final String USER_REGISTRATION_URL = "/api/register";
+    private static final String LOGIN_URL = "/api/login";
 
     // Possible request methods
     private static final String POST_METHOD = POST;
@@ -34,12 +38,14 @@ public class UserController {
     private static final String CONFIRM_PASSWORD_PARAMETER = "confirmPassword";
     private static final String ERROR_MESSAGE_PARAMETER = "errorMessage";
     private static final String MESSAGE_PARAMETER = "message";
+    private static final String TOKEN_ID_PARAMETER = "tokenId";
 
     private final UserService userService = UserServiceImpl.getInstance();
     private final HandlerRegistry handlerRegistry = HandlerRegistry.getInstance();
 
     private UserController() {
         registerUser();
+        loginUser();
     }
 
     private void registerUser() {
@@ -71,6 +77,41 @@ public class UserController {
             } finally {
                 if (log.isInfoEnabled()) {
                     log.info("Registration request successfully processed.");
+                }
+            }
+        });
+    }
+
+    private void loginUser() {
+        handlerRegistry.registerHandler(new RequestContext(LOGIN_URL, POST_METHOD), (request, response) -> {
+
+            if (log.isInfoEnabled()) {
+                log.info("Start processing login request...");
+            }
+
+            final String email = request.getParameter(EMAIL_PARAMETER);
+            final String password = request.getParameter(PASSWORD_PARAMETER);
+
+            final LoginDTO loginDTO =
+                    new LoginDTO(email, password);
+
+            final JsonEntity jsonEntity = new JsonEntity();
+            try {
+                final SecurityTokenDTO tokenDTO = userService.login(loginDTO);
+
+                jsonEntity.add(TOKEN_ID_PARAMETER, tokenDTO.getTokenId().toString());
+                jsonEntity.add(EMAIL_PARAMETER, email);
+                jsonEntity.setResponseStatusCode(SC_OK);
+            } catch (UserAuthenticationException e) {
+                jsonEntity.add(ERROR_MESSAGE_PARAMETER, e.getMessage());
+                jsonEntity.setResponseStatusCode(SC_INTERNAL_SERVER_ERROR);
+            }
+
+            try {
+                return jsonEntity;
+            } finally {
+                if (log.isInfoEnabled()) {
+                    log.info("Login request successfully processed.");
                 }
             }
         });
